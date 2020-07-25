50db7ad1e7457450b1ca5a2c05722a0335c116e2
Ethan <ebonsignori@gmail.com>
2020-07-25 13:39:01 -0400

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/build'),
+  },
+  devServer: {
+    port: 4000,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ],
+}
