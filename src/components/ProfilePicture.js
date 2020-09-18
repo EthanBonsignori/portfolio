7052bd1519273dcedffb30e7bc197990e83ebf5b
Ethan Bonsignori <ebonsignori@gmail.com>
2020-09-18 14:53:04 -0400

    add second hexagon

--- src/components/ProfilePicture.js
+++ src/components/ProfilePicture.js
@@ -4,23 +4,69 @@ import profilePic from '../assets/profile_pic.jpg';
 
 const ProfilePicture = function () {
   return (
-    <ProfilePictureContainer>
+    <>
       <HexagonContainer>
         <HexagonInner1>
           <HexagonInner2 />
         </HexagonInner1>
       </HexagonContainer>
-    </ProfilePictureContainer>
+      <HexagonBorderContainer>
+        <HexagonBorder1>
+          <HexagonBorder2>
+            <HexagonBorder3 />
+          </HexagonBorder2>
+        </HexagonBorder1>
+      </HexagonBorderContainer>
+    </>
   )
 }
 
-const ProfilePictureContainer = styled.div`
-  display: flex;
+const HexagonBorderContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -10;
 `
+
+const HexagonBorder1 = styled.div`
+  width: 400px;
+  height: 810px;
+  overflow: hidden;
+  visibility: hidden;
+  -webkit-transform: rotate(120deg);
+     -moz-transform: rotate(120deg);
+      -ms-transform: rotate(120deg);
+       -o-transform: rotate(120deg);
+          transform: rotate(120deg);  
+  
+`
+
+const HexagonBorder2 = styled.div`
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  -webkit-transform: rotate(-60deg);
+     -moz-transform: rotate(-60deg);
+      -ms-transform: rotate(-60deg);
+       -o-transform: rotate(-60deg);
+          transform: rotate(-60deg);
+`
+const HexagonBorder3 = styled.div`
+  width: 100%;
+  height: 100%;
+  visibility: visible;
+  background-position: 50%;
+  background: #FF6B6B;
+  -webkit-transform: rotate(-60deg);
+     -moz-transform: rotate(-60deg);
+      -ms-transform: rotate(-60deg);
+       -o-transform: rotate(-60deg);
+          transform: rotate(-60deg);
+`
+
 const HexagonContainer = styled.div`
   width: 400px;
   height: 800px;
-  margin: -80px 0 0 20px;
   overflow: hidden;
   visibility: hidden;
   -webkit-transform: rotate(120deg);
