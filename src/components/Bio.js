0dbbeeb73416e28cb96c6164b20aa3d42a9cdbe6
Ethan Bonsignori <ebonsignori@gmail.com>
2020-10-06 19:53:39 -0400

    bring about into bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -3,16 +3,20 @@ import styled from 'styled-components';
 import ProfilePicture from './ProfilePicture';
 import Header from './Header';
 import AboutText from './AboutText';
+import ProfilePictureBg from './ProfilePictureBg';
 
 const Bio = () => (
   <BioContainer>
     <PicturePositioner>
       <ProfilePicture />
     </PicturePositioner>
+    <ProfilePictureBg />
     <HeaderPositioner>
       <Header text={'Ethan Bonsignori\'s Portfolfio'}/>
     </HeaderPositioner>
-    <AboutText />
+    <AboutTextPositioner>
+      <AboutText />
+    </AboutTextPositioner>
   </BioContainer>
 );
 
@@ -24,12 +28,18 @@ const BioContainer = styled.div`
 
 const PicturePositioner = styled.div`
   position: relative;
+  z-index: 100;
 `;
 
 const HeaderPositioner = styled.div`
   position: absolute;
   left: 0;
   right: 0;
+`;
+
+const AboutTextPositioner = styled.div`
+  margin-top: 10rem;
+  padding: 2rem;
 `;
 
 export default Bio;
