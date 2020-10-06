9105f7c9924c43d24fd680f905fcf73f93d0cf56
Ethan Bonsignori <ebonsignori@gmail.com>
2020-10-06 19:53:53 -0400

    move profile pic background to own file

--- src/components/ProfilePictureBg.js
+++ src/components/ProfilePictureBg.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
-import profilePic from '../assets/profile_pic.jpg';
 
-const ProfilePicture = () => (
-  <>
-    <HexagonContainer>
-      <HexagonInner1>
-        <HexagonInner2 />
-      </HexagonInner1>
-    </HexagonContainer>
-  </>
+const ProfilePictureBg = () => (
+  <HexagonBorderContainer>
+    <HexagonBorder1>
+      <HexagonBorder2>
+        <HexagonBorder3 />
+      </HexagonBorder2>
+    </HexagonBorder1>
+  </HexagonBorderContainer>
 );
 
-const HexagonContainer = styled.div`
+const HexagonBorderContainer = styled.div`
+  position: absolute;
+  top: 0;
+`;
+
+const HexagonBorder1 = styled.div`
   width: 400px;
-  height: 800px;
+  height: 810px;
   overflow: hidden;
   visibility: hidden;
   -webkit-transform: rotate(120deg);
      -moz-transform: rotate(120deg);
       -ms-transform: rotate(120deg);
        -o-transform: rotate(120deg);
-          transform: rotate(120deg);
+          transform: rotate(120deg);  
+  
 `;
-const HexagonInner1 = styled.div`
+
+const HexagonBorder2 = styled.div`
   overflow: hidden;
   width: 100%;
   height: 100%;
@@ -33,14 +39,12 @@ const HexagonInner1 = styled.div`
        -o-transform: rotate(-60deg);
           transform: rotate(-60deg);
 `;
-
-const HexagonInner2 = styled.div`
+const HexagonBorder3 = styled.div`
   width: 100%;
   height: 100%;
   visibility: visible;
-  background-repeat: no-repeat;
   background-position: 50%;
-  background-image: url(${profilePic});
+  background: #FF6B6B;
   -webkit-transform: rotate(-60deg);
      -moz-transform: rotate(-60deg);
       -ms-transform: rotate(-60deg);
@@ -48,4 +52,4 @@ const HexagonInner2 = styled.div`
           transform: rotate(-60deg);
 `;
 
-export default ProfilePicture;
+export default ProfilePictureBg;
