ffb7b2b08704a0c03a7b7b065ae6ac20b5d469e8
Ethan Bonsignori <ebonsignori@gmail.com>
2020-10-06 16:38:22 -0400

    refactor Bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,68 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
 import ProfilePicture from './ProfilePicture';
-import Navigation from './Navigation';
+import Header from './Header';
+import AboutText from './AboutText';
 
-export default () => (
-  <ContentWrapper>
-    <BioContainer>
-      <Navigation />
-      <Spacer />
-      <ProfilePictureContainer>
-        <ProfilePicture />
-      </ProfilePictureContainer>
-      <Greeting>Hello,</Greeting>
-    </BioContainer>
-  </ContentWrapper>
-)
-
-const ContentWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh;
-`
+const Bio = () => (
+  <BioContainer>
+    <PicturePositioner>
+      <ProfilePicture />
+    </PicturePositioner>
+    <HeaderPositioner>
+      <Header text={'Ethan Bonsignori\'s Portfolfio'}/>
+    </HeaderPositioner>
+    <AboutText />
+  </BioContainer>
+);
 
 const BioContainer = styled.div`
-  position: relative;
-  z-index: -100;
-  width: 65vw;
-  background: #292F36;
-  border-bottom: 4px solid #FF6B6B;
-`
-
-const Spacer = styled.div`
-  height: 65vh;
-`
-
-const ProfilePictureContainer = styled.div`
-  position: absolute;
-  z-index: -1;
-  left: -200px;
-  top: -15px;
-`
-
-const Greeting = styled.div`
-  position: absolute;
-  top: 50px;
-  left: 250px;
   display: flex;
-  align-items: center;
-  padding: 10px;
-  height: 80px;
-  background-color: #70D7D0;
+  flex-direction: row;
+  position: relative;
+`;
 
-  font-family: Grenze Gotisch;
-  font-size: 80px;
-  color: #FF6B6B;
+const PicturePositioner = styled.div`
+  position: relative;
+`;
 
-  &::before {
-    content: '';
-    position: absolute;
-    right: 10px; bottom: -10px;
-    z-index: -1;
-    background: #FF6B6B;
-    width: 100%;
-    height: 100%;
-  }
-`
+const HeaderPositioner = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+`;
+
+export default Bio;
