b7d1c593035c3999c0971c495b5c79c913850407
Ethan Bonsignori <ebonsignori@gmail.com>
2020-10-06 20:37:15 -0400

    text changes

--- src/components/AboutText.js
+++ src/components/AboutText.js
@@ -5,8 +5,8 @@ const AboutText = () => (
   <About>
     <Hi>Hi,</Hi><br />
     <AboutTextContainer>
-      I&#39;m <Underline>Ethan Bonsignori</Underline> and welcome to my portfolio site.
-      I&#39;m a web developer currently living in Savannah, GA in search of a new career.
+      Welcome to my portfolio site!
+      I&#39;m a web developer currently living in Savannah, GA.
       I decided to make a change in 2019 and attend a 6-month <Underline>Full-Stack Bootcamp</Underline> offered
       by The Georgia Institute of Technology. I fell in love with the constant learning and problem solving
       that web development with JavaScript offered and dug my heels in to learning newer technologies like
@@ -18,7 +18,7 @@ const AboutText = () => (
 
 const About = styled.div`
   color: #fff;
-  font-size:  clamp(1rem, 2.5vw, 2rem);
+  font-size: clamp(1rem, 2.5vw, 2rem);
   padding: 1rem;
   border-radius: 40px;
   height: 100%;
@@ -32,12 +32,11 @@ const AboutTextContainer = styled.div`
 const Hi = styled.span`
   color: #70D7D0;
   font-family: Grenze Gotisch;
-  font-size:  clamp(2rem, 3vw, 3rem);
+  font-size: clamp(2rem, 3vw, 3rem);
 `;
 
 const Underline = styled.span`
   text-decoration: underline #70D7D0;
-  /* font-size:  clamp(1.5rem, 3vw, 3rem); */
 `;
 
 export default AboutText;
