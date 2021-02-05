0a63556c8dc8526353a643b5b987069213be7f22
Ethan Bonsignori <ebonsignori@gmail.com>
2021-02-05 13:27:04 -0500

    refactor contact page

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,55 +1,87 @@
 import React from 'react';
 import styled from 'styled-components';
-import EmailOutlined from '@material-ui/icons/EmailOutlined';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Headline from '../PageHeadline';
+import ContactForm from '../ContactForm';
 import { breakpoints } from '../../utils/styleUtils';
 
 const Contact = () => (
   <>
-    <Headline>CONTACT ME</Headline>
-    <ContactCards>
-      <ContactLink
-        target='_blank'
-        rel="noreferrer noopener"
-        href='mailto:ebonsignori@gmail.com'
-      >
-        <ContactCard>
-          <EmailOutlined />
-          Email
-        </ContactCard>
-      </ContactLink>
-      <ContactLink
-        target='_blank'
-        rel="noreferrer noopener"
-        href='https://www.github.com/EthanBonsignori/'
-      >
-        <ContactCard>
-          <GitHubIcon />
-          GitHub
-        </ContactCard>
-      </ContactLink>
-      <ContactLink
-        target='_blank'
-        rel="noreferrer noopener"
-        href='https://www.linkedin.com/in/EthanBonsignori/'
-      >
-        <ContactCard>
-          <LinkedInIcon />
-          LinkedIn
-        </ContactCard>
-      </ContactLink>
-    </ContactCards>
+    <Headline>CONTACT</Headline>
+    <ContactForm />
+    <CardsWrapper>
+      <CardsPositioner>
+        <CardsTitle>CONNECT</CardsTitle>
+        <ContactLink
+          title='Email'
+          target='_blank'
+          rel="noreferrer noopener"
+          href='mailto:ebonsignori@gmail.com'
+        >
+          <ContactCard>
+            <FontAwesomeIcon icon='envelope' />
+          </ContactCard>
+        </ContactLink>
+        <ContactLink
+          title='GitHub'
+          target='_blank'
+          rel="noreferrer noopener"
+          href='https://www.github.com/EthanBonsignori/'
+        >
+          <ContactCard>
+            <FontAwesomeIcon icon={['fab', 'github']} />
+          </ContactCard>
+        </ContactLink>
+        <ContactLink
+          title='LinkedIn'
+          target='_blank'
+          rel="noreferrer noopener"
+          href='https://www.linkedin.com/in/EthanBonsignori/'
+        >
+          <ContactCard>
+            <FontAwesomeIcon icon={['fab', 'linkedin']} />
+          </ContactCard>
+        </ContactLink>
+        <ContactLink
+          title='Facebook'
+          target='_blank'
+          rel="noreferrer noopener"
+          href='https://www.facebook.com/EthanBonsignori/'
+        >
+          <ContactCard>
+            <FontAwesomeIcon icon={['fab', 'facebook']} />
+          </ContactCard>
+        </ContactLink>
+      </CardsPositioner>
+    </CardsWrapper>
   </>
 );
 
-const ContactCards = styled.div`
+const CardsWrapper = styled.div`
   display: flex;
+  justify-content: center;
   width: 100%;
+  margin-top: 2em;
+
+`;
+
+const CardsPositioner = styled.div`
+  display: flex;
   flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 60%;
+
+  border-top: 1px solid #fff;
+
+  ${breakpoints.mobile} {
+    width: 100%;
+  }
+`;
+
+const CardsTitle = styled.div`
+
 `;
 
 const ContactCard = styled.div`
@@ -57,30 +89,30 @@ const ContactCard = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  border-radius: 5px;
-  padding: 5em;
   margin: 1em;
 
-  border: 1px solid ${({ theme }) => theme.color.salmon};
   color: ${({ theme }) => theme.color.neonBlue};
 
   svg {
-    font-size: 3em;
+    font-size: 1.5em;
   }
 
-  &:hover,
-  &:active {
-    background-color: ${({ theme }) => theme.color.activeTab};
-  }
+
 
   ${breakpoints.mobile} {
     width: 100%;
+    margin: 1em 0;
   }
 `;
 
 const ContactLink = styled.a`
   color: #fff;
   text-decoration: none;
+
+  &:hover,
+  &:active {
+    opacity: 0.5;
+  }
 `;
 
 export default Contact;
