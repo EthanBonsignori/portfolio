f9fd7372f2f333e127b7607346196a074d2d6723
Ethan Bonsignori <ebonsignori@gmail.com>
2021-02-05 13:47:20 -0500

    add temp comment for deploy

--- src/components/Pages/Blog.js
+++ src/components/Pages/Blog.js
@@ -5,6 +5,7 @@ import Headline from '../PageHeadline';
 const Blog = () => (
   <>
     <Headline>BLOG</Headline>
+    Actively being worked on right now. Check back later, thanks.
   </>
 );
 
--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -5,6 +5,7 @@ import Headline from '../PageHeadline';
 const Projects = () => (
   <>
     <Headline>PROJECTS</Headline>
+    Actively being worked on right now. Check back later, thanks.
   </>
 );
 
