4f550312afd3b4cc517427ffc50018690ad1a784
Ethan Bonsignori <ebonsignori@gmail.com>
2021-02-05 13:26:17 -0500

    import icons

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import GlobalStyle from './GlobalStyle';
 import Theme from './Theme';
 import Main from './Main';
+
+library.add(
+  fab,
+  faEnvelope
+);
 
 const App = () => (
   <>
