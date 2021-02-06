4b1988de7f308222543eb0d363f1a1574935b45a
Ethan Bonsignori <ebonsignori@gmail.com>
2021-02-06 14:20:28 -0500

    add keyframes

---
+++ src/utils/keyframes.js
@@ -0,0 +1,12 @@
+import { keyframes } from 'styled-components';
+
+export const drawBorder = keyframes`
+  from {
+    width: 20px;
+  }
+  to {
+    width: 100%:
+  }
+`;
+
+export const placeholder = '';
