829f6c92f277bcf931986522aea50f58b04a3a0c
Ethan Bonsignori <ebonsignori@gmail.com>
2021-02-24 14:42:28 -0500

    add animations

--- src/utils/keyframes.js
+++ src/utils/keyframes.js
@@ -18,4 +18,66 @@ export const pulse = keyframes`
   }
 `;
 
-export const placeholder = '';
+export const popIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+
+  99% {
+    opacity: 0;
+  }
+
+  100% {
+    opactiy: 1;
+  }
+`;
+
+export const fadeIn = keyframes`
+  0% {
+    opacity 0;
+  }
+
+  60% {
+    opacity: 0;
+  }
+  
+  100% {
+    opacity: 1;
+  }
+`;
+
+export const logoSlide = keyframes`
+  0% {
+    top: 50%;
+    left: 20px;
+  }
+
+  99% {
+    opacity: 1;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  
+  100% {
+    opacity: 0;
+  }
+`;
+
+export const titleSlide = keyframes`
+  0% {
+    top: 50%;
+    left: 100px;
+  }
+  
+  99% {
+    opacity 1;
+    top: 83px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  100% {
+    opacity: 0;
+  }
+`;
