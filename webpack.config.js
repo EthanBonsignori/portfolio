22a060b0eb77e9f7970913b47ac3583e793fb2f0
Ethan Bonsignori <ebonsignori@gmail.com>
2021-02-24 20:06:47 -0500

    add css loader for carousel

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
         },
       },
       {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.(png|svg|jpg|gif|ico)$/,
         use: [
           'file-loader',
