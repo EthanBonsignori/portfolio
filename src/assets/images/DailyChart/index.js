51b1439b2d1512e41e2ff890ef90f6ea4602c8c4
Ethan Bonsignori <ebonsignori@gmail.com>
2021-02-24 20:06:24 -0500

    add images

---
+++ src/assets/images/DailyChart/index.js
@@ -0,0 +1,7 @@
+import dailychart1 from './1.png';
+import dailychart2 from './2.gif';
+
+export {
+  dailychart1,
+  dailychart2,
+};
---
+++ src/assets/images/eBridge/index.js
@@ -0,0 +1,15 @@
+import ebridge1 from './1.png';
+import ebridge2 from './2.png';
+import ebridge3 from './3.png';
+import ebridge4 from './4.png';
+import ebridge5 from './5.png';
+import ebridge6 from './6.png';
+
+export {
+  ebridge1,
+  ebridge2,
+  ebridge3,
+  ebridge4,
+  ebridge5,
+  ebridge6,
+};
