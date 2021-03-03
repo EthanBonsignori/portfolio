e28b71337b2f1e436063ce9cc6741262c32ad3a7
Ethan Bonsignori <ebonsignori@gmail.com>
2021-03-02 21:49:09 -0500

    add author and category

--- src/assets/blogPosts.js
+++ src/assets/blogPosts.js
@@ -3,7 +3,9 @@ import mydeveloperstory from './posts/mydeveloperstory.md';
 const posts = [
   {
     title: 'My Developer Story',
-    createdAt: '2021-02-28T20:43:25.549Z',
+    author: 'Ethan Bonsignori',
+    category: 'Life',
+    createdAt: 'February 28, 2021',
     blogLink: 'mydeveloperstory',
     mdLink: mydeveloperstory,
   },
