73391c923e7a8e55d2738b92c4861f628813cae7
Ethan Bonsignori <ebonsignori@gmail.com>
2021-03-03 19:11:54 -0500

    add back button

--- src/components/Pages/Blog.js
+++ src/components/Pages/Blog.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { useParams, Outlet } from 'react-router-dom';
+import { Link, useParams, Outlet } from 'react-router-dom';
+import styled from 'styled-components';
 import Headline from './shared/Headline';
+import BackButton from './shared/BackButton';
 import MarkdownRenderer from '../MarkdownRenderer';
 import blogPosts from '../../assets/blogPosts';
+import { fadeIn } from '../../utils/keyframes';
 
 const Blog = ({ darkMode, toggleTheme }) => {
   const { blogLink } = useParams();
@@ -12,10 +15,21 @@ const Blog = ({ darkMode, toggleTheme }) => {
   return (
     <>
       <Headline title='BLOG' darkMode={darkMode} toggleTheme={toggleTheme} />
-      <MarkdownRenderer content={blog} />
+      <BlogWrapper>
+        <MarkdownRenderer content={blog} />
+      </BlogWrapper>
+      <Link to='/blog'>
+        <BackButton>&#8592;&nbsp;Back to Blog</BackButton>
+      </Link>
       <Outlet />
     </>
   );
 };
+
+const BlogWrapper = styled.div`
+  opacity: 0;
+  animation: ${fadeIn} 1s forwards;
+  animation-delay: 50ms;
+`;
 
 export default Blog;
