9ed8c1c5ec6ae7322f70eda75da97c66b2f603df
Ethan Bonsignori <ebonsignori@gmail.com>
2021-03-03 19:12:42 -0500

    add category sort

--- src/components/Pages/BlogList.js
+++ src/components/Pages/BlogList.js
@@ -3,12 +3,13 @@ import { Link, Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
+import breakpoints from '../../utils/breakpoints';
 import Headline from './shared/Headline';
 import blogPosts from '../../assets/blogPosts';
 
 const BlogList = ({ darkMode, toggleTheme }) => {
   const [dateSort, setDateSort] = useState('descending');
-  // const [tagSort, setTagSort] = useState('all');
+  const [categorySort, setCategorySort] = useState('all');
 
   const handleDateSort = () => {
     if (dateSort === 'descending') {
@@ -25,17 +26,22 @@ const BlogList = ({ darkMode, toggleTheme }) => {
       sortedPosts = blogPosts.sort((a, b) => Date.parse(a.createdAt) - Date.parse(b.createdAt));
     }
 
+    if (categorySort !== 'all') {
+      sortedPosts = blogPosts.filter(post => post.category.toLowerCase() === categorySort);
+    }
+
     return sortedPosts.map((b, i) => (
       <BlogPost key={i}>
         <BlogPostHeader>
           <Link to={b.blogLink}>
             {b.title}
           </Link>
-          <span>
-            {new Date(b.createdAt).toLocaleDateString('en-US')}
-          </span>
+          <BlogDate>{b.createdAt}</BlogDate>
         </BlogPostHeader>
-        ⟶
+        <BlogDetails>
+          <span>⟶&nbsp;{b.category}</span>
+          <span>{b.author}</span>
+        </BlogDetails>
       </BlogPost>
     ));
   };
@@ -45,12 +51,20 @@ const BlogList = ({ darkMode, toggleTheme }) => {
       <BlogPostsWrapper>
         <SortWrapper>
           <SortButton title={dateSort} onClick={handleDateSort}>
-            Date&nbsp;
+            Sort By Date&nbsp;
             {dateSort === 'descending'
               ? <FontAwesomeIcon icon={faCaretDown}/>
               : <FontAwesomeIcon icon={faCaretUp} />
             }
           </SortButton>
+          <SelectWrapper>
+            <label htmlFor='category'>Category: </label>
+            <SortSelect id='category' value={categorySort} onChange={e => setCategorySort(e.target.value)}>
+              <option value="all">All</option>
+              <option value="technology">Technology</option>
+              <option value="life">Life</option>
+            </SortSelect>
+          </SelectWrapper>
         </SortWrapper>
         {getBlogsJsx()}
       </BlogPostsWrapper>
@@ -62,12 +76,26 @@ const BlogList = ({ darkMode, toggleTheme }) => {
 const BlogPost = styled.div`
   display: flex;
   flex-direction: column;
-  margin: 1em 0;
+  margin: 0.2em 0;
+  background-color: ${({ theme }) => theme.color.cardBackground};
+  padding: 0.5em;
 `;
 const BlogPostHeader = styled.div`
   display: flex;
   font-size: 1.3em;
   width: 100%;
+  justify-content: space-between;
+
+  ${breakpoints.mobile} {
+    font-size: 1.2em;
+    flex-wrap: wrap;
+  }
+`;
+const BlogDate = styled.span`
+  font-size: 0.8em;
+`;
+const BlogDetails = styled.div`
+  display: flex;
   justify-content: space-between;
 `;
 
@@ -85,6 +113,18 @@ const SortButton = styled.button`
   cursor: pointer;
   background: none;
   border: none;
+`;
+const SelectWrapper = styled.div`
+  font-size: 0.8em;
+`;
+const SortSelect = styled.select`
+  color: ${({ theme }) => theme.color.text};
+  background: ${({ theme }) => theme.color.cardBackground};
+  border: 1px solid ${({ theme }) => theme.color.cardBackground};
+
+  option {
+    background: ${({ theme }) => theme.color.cardBackground};
+  }
 `;
 
 export default BlogList;
