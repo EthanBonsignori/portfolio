92ff622fe5993e2f94b8bea9c703ba60f05846ec
Ethan Bonsignori <ebonsignori@gmail.com>
2021-03-05 18:36:33 -0500

    add blog post routes

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,57 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 const express = require('express');
-const cors = require('cors');
 
-const routes = require('./routes');
-
+// Init Express App
 const app = express();
 
-app.use(cors({ origin: true }));
-app.use('/api', routes);
+admin.initializeApp();
 
-exports.app = functions.https.onRequest(app);
+exports.blog = functions.https.onRequest(app);
+
+const db = admin.firestore();
+
+const blogCol = db.collection('blog');
+const increment = admin.firestore.FieldValue.increment(1);
+const decrement = admin.firestore.FieldValue.increment(-1);
+
+function createNewDoc(blog) {
+  db.collection('blog').doc(blog).set({ likes: 0 });
+}
+
+app.get('/:blog', (req, res) => {
+  const { blog } = req.params;
+  db.collection('blog').doc(blog).get().then(doc => {
+    if (doc.exists) {
+      return res.status(200).json({ likes: doc.likes });
+    }
+    createNewDoc(blog);
+    return res.status(200).json({ likes: 0 });
+  })
+    .catch(error => res.status(500).json({ message: 'Error in GET /:blog', error }));
+});
+
+app.put('/:blog', (req, res) => {
+  const { blog } = req.params;
+  db.collection('blog').doc(blog).get().then(doc => {
+    if (doc.exists) {
+      blogCol.doc(blog).update({ likes: increment });
+      return res.status(200).json({ success: true });
+    }
+    createNewDoc(blog);
+    return res.status(200).json({ success: true });
+  })
+    .catch(error => res.status(500).json({ message: 'Error in PUT /:blog', error }));
+});
+
+app.put('/:blog/unlike', (req, res) => {
+  const { blog } = req.params;
+  db.collection('blog').doc(blog).get().then(doc => {
+    if (doc.exists) {
+      blogCol.doc(blog).update({ likes: decrement });
+      return res.status(200).json({ success: true });
+    }
+    return res.status(200).json({ success: true });
+  })
+    .catch(error => res.status(500).json({ message: 'Error in PUT /:blog/unlike', error }));
+});
