d5720c6c21af63821d2c6d108a51f7bbc49380ef
Ethan Bonsignori <ebonsignori@gmail.com>
2021-03-08 16:09:04 -0500

    add font support for all browsers

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,24 +1,35 @@
 import { createGlobalStyle } from 'styled-components';
 import NothingYouCouldDoWoff from '../assets/fonts/nothing-you-could-do.woff';
 import NothingYouCouldDoWoff2 from '../assets/fonts/nothing-you-could-do.woff2';
-import GrenzeGotischWoff from '../assets/fonts/Grenze_Gotisch.woff';
-import GrenzeGotischWoff2 from '../assets/fonts/Grenze_Gotisch.woff2';
-import PiazollaWoff from '../assets/fonts/Piazolla.woff';
-import PiazollaWoff2 from '../assets/fonts/Piazolla.woff2';
+import Piazzolla600eot from '../assets/fonts/piazzolla-v8-latin-600.eot';
+import Piazzolla600woff2 from '../assets/fonts/piazzolla-v8-latin-600.woff2';
+import Piazzolla600woff from '../assets/fonts/piazzolla-v8-latin-600.woff';
+import Piazzolla600ttf from '../assets/fonts/piazzolla-v8-latin-600.ttf';
+import Piazzolla600svg from '../assets/fonts/piazzolla-v8-latin-600.svg';
+import Piazzolla800eot from '../assets/fonts/piazzolla-v8-latin-800.eot';
+import Piazzolla800woff2 from '../assets/fonts/piazzolla-v8-latin-800.woff2';
+import Piazzolla800woff from '../assets/fonts/piazzolla-v8-latin-800.woff';
+import Piazzolla800ttf from '../assets/fonts/piazzolla-v8-latin-800.ttf';
+import Piazzolla800svg from '../assets/fonts/piazzolla-v8-latin-800.svg';
+import Piazzolla600Italiceot from '../assets/fonts/piazzolla-v8-latin-600italic.eot';
+import Piazzolla600Italicwoff2 from '../assets/fonts/piazzolla-v8-latin-600italic.woff2';
+import Piazzolla600Italicwoff from '../assets/fonts/piazzolla-v8-latin-600italic.woff';
+import Piazzolla600Italicttf from '../assets/fonts/piazzolla-v8-latin-600italic.ttf';
+import Piazzolla600Italicsvg from '../assets/fonts/piazzolla-v8-latin-600italic.svg';
 
 const GlobalStyle = createGlobalStyle`
   html, * {
     transition: background-color 500ms ease-in-out;
   }
   body {
-    font-family: 'Piazolla';
+    font-family: 'Piazzolla';
     background: ${({ theme }) => theme.color.background};
     margin: 0;
   }
   div, a, p, input, textarea, button, 
   h1, h2, h3, h4, h5, h6 {
     color: ${({ theme }) => theme.color.text};
-    font-family: 'Piazolla';
+    font-family: 'Piazzolla';
     box-sizing: border-box;
   }
   @font-face {
@@ -30,23 +41,46 @@ const GlobalStyle = createGlobalStyle`
     font-style: normal;
   }
 
-  @font-face {
-    font-family: 'Grenze Gotisch';
-    src: local('Grenze Gotisch'), local('GrenzeGotisch'),
-    url(${GrenzeGotischWoff2}) format('woff2'),
-    url(${GrenzeGotischWoff}) format('woff');
-    font-weight: 600;
-    font-style: normal;
-  }
+/* piazzolla-600 - latin */
+@font-face {
+  font-family: 'Piazzolla';
+  font-style: normal;
+  font-weight: 600;
+  src: url(${Piazzolla600eot}); /* IE9 Compat Modes */
+  src: local(''),
+       url(${Piazzolla600eot}) format('embedded-opentype'), /* IE6-IE8 */
+       url(${Piazzolla600woff2}) format('woff2'), /* Super Modern Browsers */
+       url(${Piazzolla600woff}) format('woff'), /* Modern Browsers */
+       url(${Piazzolla600ttf}) format('truetype'), /* Safari, Android, iOS */
+       url(${Piazzolla600svg}) format('svg'); /* Legacy iOS */
+}
+/* piazzolla-800 - latin */
+@font-face {
+  font-family: 'Piazzolla';
+  font-style: bold;
+  font-weight: 800;
+  src: url(${Piazzolla800eot}); /* IE9 Compat Modes */
+  src: local(''),
+       url(${Piazzolla800eot}) format('embedded-opentype'), /* IE6-IE8 */
+       url(${Piazzolla800woff2}) format('woff2'), /* Super Modern Browsers */
+       url(${Piazzolla800woff}) format('woff'), /* Modern Browsers */
+       url(${Piazzolla800ttf}) format('truetype'), /* Safari, Android, iOS */
+       url(${Piazzolla800svg}) format('svg'); /* Legacy iOS */
+}
+/* piazzolla-600italic - latin */
+@font-face {
+  font-family: 'Piazzolla';
+  font-style: italic;
+  font-weight: 600;
+  src: url(${Piazzolla600Italiceot}); /* IE9 Compat Modes */
+  src: local(''),
+       url(${Piazzolla600Italiceot}) format('embedded-opentype'), /* IE6-IE8 */
+       url(${Piazzolla600Italicwoff2}) format('woff2'), /* Super Modern Browsers */
+       url(${Piazzolla600Italicwoff}) format('woff'), /* Modern Browsers */
+       url(${Piazzolla600Italicttf}) format('truetype'), /* Safari, Android, iOS */
+       url(${Piazzolla600Italicsvg}) format('svg'); /* Legacy iOS */
+}
 
-  @font-face {
-    font-family: 'Piazolla';
-    src: local('Piazolla'),
-    url(${PiazollaWoff2}) format('woff2'),
-    url(${PiazollaWoff}) format('woff');
-    font-weight: 600;
-    font-style: normal;
-  }
 `;
 
 export default GlobalStyle;
