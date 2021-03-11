dd5e5f78464b9964804463015b90f1af38a22eb8
Ethan Bonsignori <ebonsignori@gmail.com>
2021-03-11 18:51:16 -0500

    add landscape breakpoint

--- src/utils/breakpoints.js
+++ src/utils/breakpoints.js
@@ -2,6 +2,7 @@ export const size = {
   mobile: '350px',
   mobileLg: '500px',
   tablet: '768px',
+  landscape: '1450px',
   desktop: '1920px',
   desktopLg: '2560px',
 };
@@ -9,6 +10,7 @@ export const size = {
 const breakpoints = {
   mobile: `@media (max-width: ${size.tablet}) and (min-width: 1px)`,
   mobileLg: `@media (max-width: ${size.tablet}) and (min-width: ${size.mobileLg})`,
+  landscape: `@media (max-width: ${size.landscape}) and (min-width: ${size.tablet})`,
   desktop: `@media (max-width: ${size.desktop}) and (min-width: ${size.tablet})`,
   desktopLg: `@media (min-width: ${size.desktopLg})`,
 };
