c87c2414b24d4fe29b3263c0b518f4c01527cf9f
Ethan Bonsignori <ebonsignori@gmail.com>
2021-04-05 16:53:22 -0400

    move contact form to home

---
+++ src/components/Pages/Design.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Headline from './shared/Headline';
+
+const Design = ({ darkMode, toggleTheme }) => (
+  <>
+    <Headline title='DESIGN' darkMode={darkMode} toggleTheme={toggleTheme} />
+    Work Coming Soon
+  </>
+);
+
+export default Design;
