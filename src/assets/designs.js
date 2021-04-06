f4df964b6c57155c751d698f8c346dda778ee08e
Ethan Bonsignori <ebonsignori@gmail.com>
2021-04-06 11:20:36 -0400

    add design page

---
+++ src/assets/designs.js
@@ -0,0 +1,57 @@
+export default [
+  {
+    name: 'Miles of Reflection',
+    url: 'https://i.imgur.com/fMkGrQZ.png',
+    type: 'Logo',
+    description: '',
+  },
+  {
+    name: 'Enchant My Party',
+    url: 'https://i.imgur.com/Q2xgYbh.png?1',
+    type: 'Rack Card',
+    description: '',
+  },
+  {
+    name: 'A Midsummer Night\'s Dream',
+    url: 'https://i.imgur.com/6nXnf8A.png',
+    type: 'Rack Card',
+    description: '',
+  },
+  {
+    name: 'Peter and the Starcatcher',
+    url: 'https://i.imgur.com/Vg5iCZT.png',
+    type: 'Rack Card',
+    description: '',
+  },
+  {
+    name: 'The Servant of Two Masters',
+    url: 'https://i.imgur.com/3IPl0fV.jpg',
+    type: 'Rack Card',
+    description: '',
+  },
+  {
+    name: 'Stellaluna',
+    url: 'https://i.imgur.com/ecBq80e.png',
+    type: 'Rack Card',
+    description: '',
+  },
+  {
+    name: 'Mary Poppins',
+    url: 'https://i.imgur.com/FVgfzTV.png',
+    type: 'Rack Card',
+    description: '',
+  },
+  {
+    name: 'Beauty and the Beast',
+    url: 'https://i.imgur.com/9VIqjMf.png',
+    type: 'Event Card',
+    description: '',
+  },
+  {
+    name: 'GifSearch',
+    url: 'https://i.imgur.com/7B8QDA4.png',
+    type: 'Logo',
+    description: '',
+  },
+
+];
