bc832c63acc93019fbca5ab9c8356f66301241af
Ethan Bonsignori <ebonsignori@gmail.com>
2024-03-11 01:37:22 -0700

    update and fix functions

--- functions/index.js
+++ functions/index.js
@@ -1,74 +1,54 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const express = require('express');
-const cors = require('cors')({ origin: true });
+const { onRequest } = require('firebase-functions/v2/https');
+const { initializeApp } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
 
 // Init Express App
-const app = express();
-app.use(cors);
+initializeApp();
+const db = getFirestore();
 
-admin.initializeApp();
-
-exports.blog = functions.https.onRequest(app);
-
-const db = admin.firestore();
-
-const blogCol = db.collection('blog');
-const increment = admin.firestore.FieldValue.increment(1);
-const decrement = admin.firestore.FieldValue.increment(-1);
-
-function createNewDoc(blog) {
+async function createNewDoc(blog) {
   db.collection('blog').doc(blog).set({ likes: 0 });
 }
 
-app.get('/:blog', (req, res) => {
-  const { blog } = req.params;
+exports.getBlog = onRequest(async (req, res) => {
+  const blog = req.params[0];
   db.collection('blog')
     .doc(blog)
     .get()
     .then((doc) => {
-      if (doc.exists) {
-        return res.status(200).json(doc.data());
+      if (!doc.exists) {
+        createNewDoc(blog);
+        return res.status(200).json({ success: true, likes: doc.data().likes });
       }
-      createNewDoc(blog);
-      return res.status(200).json({ likes: 0 });
+      return res.status(200).json({ success: true, likes: doc.data().likes });
     })
-    .catch((error) =>
-      res.status(500).json({ message: 'Error in GET /:blog', error }),
-    );
+    .catch((error) => {
+      return res.status(500).json({
+        message: 'Error in blog onRequest /:blog',
+        error: error.message,
+      });
+    });
 });
 
-app.put('/:blog', (req, res) => {
-  const { blog } = req.params;
+exports.likeBlog = onRequest(async (req, res) => {
+  const blog = req.params[0];
   db.collection('blog')
     .doc(blog)
     .get()
     .then((doc) => {
-      if (doc.exists) {
-        blogCol.doc(blog).update({ likes: increment });
-        return res.status(200).json({ success: true });
+      if (!doc.exists) {
+        createNewDoc(blog);
+        return res
+          .status(200)
+          .json({ success: true, likes: doc.data().likes + 1 });
       }
-      createNewDoc(blog);
-      return res.status(200).json({ success: true });
+      doc.ref.update({ likes: doc.data().likes + 1 });
+      return res.status(200).json({ success: true, likes: doc.data().likes });
     })
-    .catch((error) =>
-      res.status(500).json({ message: 'Error in PUT /:blog', error }),
-    );
-});
-
-app.put('/:blog/unlike', (req, res) => {
-  const { blog } = req.params;
-  db.collection('blog')
-    .doc(blog)
-    .get()
-    .then((doc) => {
-      if (doc.exists) {
-        blogCol.doc(blog).update({ likes: decrement });
-        return res.status(200).json({ success: true });
-      }
-      return res.status(200).json({ success: true });
-    })
-    .catch((error) =>
-      res.status(500).json({ message: 'Error in PUT /:blog/unlike', error }),
-    );
+    .catch((error) => {
+      return res.status(500).json({
+        message: 'Error in blog onRequest /:blog',
+        error: error.message,
+      });
+    });
 });
